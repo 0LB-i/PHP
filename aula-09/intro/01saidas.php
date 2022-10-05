ca29934a15731d8a7c931aae45305250daa4ec28
GABRIEL BRANDAO MACHADO <gabriel.machado4@acad.univates>
2022-10-04 22:20:27 -0300

    Aula 10

--- aula-09/intro/01saidas.php
+++ aula-09/intro/01saidas.php
@@ -48,20 +48,46 @@ print "<h4>Escreva: {$hello}</h4>";
  */
 fullStackPHPClassSession("print_r", __LINE__);
 
+//imprime um array
+
+$array = ["Teotônia", "Lajeado", "Tabaí"];
+
+print_r($array);
+
+$vetor = print_r($array,true); //conm o true ele não imprime na tela, e devolve o resultado para outra variável
+
+echo "<br>";
+print($vetor);
+
+echo "<br>";
+echo "<pre>".$vetor."</pre>";
 
 /**
  * [ printf ] https://php.net/manual/pt_BR/function.printf.php
  */
 fullStackPHPClassSession("printf", __LINE__);
 
+$article = "<article><h2>%s</h2><p>%s</p></article>";
+
+$titulo = "Título da notcía";
+$noticia = "Em linguística, a noção de texto é ampla e ainda aberta a uma definição mais precisa. Grosso modo, pode ser entendido como manifestação linguística das ideias de um autor.";
+
+printf($article,$titulo,$noticia);
 
 /**
  * [ vprintf ] https://php.net/manual/pt_BR/function.vprintf.php
  */
-fullStackPHPClassSession("vprintf", __LINE__);
+fullStackPHPClassSession("vprintf", __LINE__); //é igual ao printf mas possibilita mandar um array tbm
 
+$company = "<article><h2>Instituição: %s</h2><p>Curso: %s</p></article>";
+
+$array_company = ["UNIVATES","Técnico em Informática"];
+
+vprintf($company, $array_company);
 
 /**
  * [ var_dump ] https://php.net/manual/pt_BR/function.var-dump.php
  */
 fullStackPHPClassSession("var_dump", __LINE__);
+
+var_dump($company);
--- aula-09/intro/02variaveis.php
+++ aula-09/intro/02variaveis.php
@@ -8,20 +8,80 @@ fullStackPHPClassName("Variáveis e tipos de dados");
  */
 fullStackPHPClassSession("variáveis", __LINE__);
 
+$nome = 'Gabriel';
+$idade = 19;
+$cidade = 'Tabaí';
+
+echo "<h4>Meu nome é {$nome}, tenho {$idade} anos de idade e moro em {$cidade}.</h4>";
+
+$pre_formatado = "<h4>Meu nome é %s, tenho %d anos de idade e moro em %s.</h4>";
+
+printf($pre_formatado, $nome, $idade, $cidade);
+
+var_dump([
+    "Variável" => $idade
+]);
 
 /**
  * [ tipo boleano ] true | false
  */
 fullStackPHPClassSession("tipo boleano", __LINE__);
 
+$true = true;
+$false = false;
+
+$idade_pessoa = 19;
+
+$maior_idade = ($idade_pessoa >= 18);
+
+echo "Maior de Idade = " . ($maior_idade ? "SIM" : "NÃO");
+
+var_dump([$true, $false, $maior_idade]);
 
 /**
  * [ tipo callback ] call | closure
  */
 fullStackPHPClassSession("tipo callback", __LINE__);
 
+$code = "<h3>Titulo H3</h3><h4>Titulo H4</h4>";
+
+$cleanCode = call_user_func("strip_tags", $code);
+
+echo $code;
+echo $cleanCode;
+
+var_dump([
+    'code' => $code,
+    'cleanCode' => $cleanCode
+]);
+
+$retorno = function($param){
+    echo "Saída = " . $param;
+};
+
+$retorno("UNIVATES");
 
 /**
  * [ outros tipos ] string | array | objeto | numérico | null
  */
 fullStackPHPClassSession("outros tipos", __LINE__);
+
+$string = "Gabriel";
+$array = array("Olá","Mundo");
+$objeto = new stdClass();
+$objeto -> nome = $string;
+
+$int = 10;
+$float = 127.989;
+$null = null;
+
+echo "<pre>";
+    var_dump([
+        $string,
+        $array,
+        $objeto,
+        $int,
+        $float,
+        $null
+    ]);
+echo "</pre>";
