30c7cc7e0d327cbf60ce7d6d7d0c3f40dc9ed071
OLB <OLB.wtd@gmail.com>
2023-01-10 11:32:10 -0300

    PHP Univates

---
+++ aula-09/atividade.php
@@ -0,0 +1,12 @@
+<h1>Atividade</h1>
+<?php
+
+$texto = 'Texto da atividade';
+
+echo "<h1>{$texto}</h1>";
+
+echo "<h1>" . $texto . "</h1>";
+
+print "<h1>$texto, " . "$texto</h1>";
+
+print("<h1>$texto</h1>");
